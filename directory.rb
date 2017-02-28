e51749ed739ffd8ccf585fd57a45f4c9aa4c3213
Edward Ward <edwardwardward@gmail.com>
2017-02-28 18:57:53 +0000

    Add try_load_students method

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv" 
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -34,18 +34,18 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? do
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -60,13 +60,25 @@ def save_students
   file.close
 end
 
-def load_students
-  file =File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file =File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil? #get out of method if no file entered
+  if File.exists?(filename)
+    load_students(filename)
+      puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry #{filename} doesn't exist"
+    exit
+  end
 end
 
 def print_header
@@ -84,8 +96,5 @@ def print_footer(names)
   puts "Overall we have #{names.count} great students"
 end
 
+try_load_students
 interactive_menu()
-
-print_header
-print(@students)
-print_footer(@students)
