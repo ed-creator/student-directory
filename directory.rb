6e23a2b8c811f4a9b6b9b511e032d0e85f76435a
Edward Ward <edwardwardward@gmail.com>
2017-02-28 17:31:50 +0000

    Refactor interactive menu

--- directory.rb
+++ directory.rb
@@ -1,41 +1,46 @@
+@students = []
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list(@students)
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit #this will terminate the program
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
 def interactive_menu
-  students = []
   loop do
-    #1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-
-
-    puts "9. Exit"
-    #2. read the input into a variable
-    selection = gets.chomp
-    #3. do what the user has asked
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        print_header
-        print(students)
-        print_footer(students)
-      when "9"
-        exit #this will terminate the program
-      else
-        puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
 end
 
 def print_header
@@ -43,7 +48,7 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
+def print_students_list(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]}) cohort"
   end
@@ -56,5 +61,5 @@ end
 interactive_menu()
 
 print_header
-print(students)
-print_footer(students)
+print(@students)
+print_footer(@students)
